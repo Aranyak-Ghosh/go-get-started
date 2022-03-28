6ca7037863379b9775dd02c1ed3b951c3f484523
Aranyak Ghosh <aranyakghosh@gmail.com>
2022-03-28 17:00:33 +0400

    i am a genius

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/Aranyak-Ghosh/go-get-started
 
-go 1.17
+go 1.18
--- main.go
+++ main.go
@@ -9,18 +9,39 @@ type sample struct {
 	A int64 `json:"a"`
 }
 
+type SampleArray[T interface{}] []T
+
+func (s *SampleArray[T]) UnmarshalJSON(data []byte) error {
+	var out []T
+	err := json.Unmarshal(data, &out)
+	if err != nil {
+		return err
+	} else {
+		*s = out
+		return nil
+	}
+}
+
+// Map, foreach, push, pop, removeAt, filter, 
+
 func main() {
-	response := []byte(`
+	response := []byte(`[
 	{
 		"a": 123
-	}`)
+	}
+	]`)
 
-	var out sample
+	var out SampleArray[sample]
+
+	l := len(out)
+
+	fmt.Println(l)
+
 	err := json.Unmarshal(response, &out)
 	if err != nil {
 		fmt.Println(err.Error())
 	} else {
-		dat, _ := json.Marshal(out)
+		dat, _ := json.Marshal(out[0])
 		fmt.Printf(string(dat))
 	}
 }
